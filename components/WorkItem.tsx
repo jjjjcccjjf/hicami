dfd65aa24ee41e150c5369c2ea0f67af9216801a
Ro11ingButler <rollingkai1214@gmail.com>
2023-03-27 03:30:34 +0800

    Fix accordion

--- components/WorkItem.tsx
+++ components/WorkItem.tsx
@@ -1,13 +1,14 @@
 import FadeInSection from "./FadeInSection"
 import useUrlFor from "@/hooks/useUrlFor"
 
-export default function WorkItem({ item }: any) {
+export default function WorkItem({ item, className}: any) {
 
     const imageUrl = useUrlFor(item.image).quality(80).url()
 
     return (
         <>
             <img
+                className={className}
                 src={imageUrl}
                 srcSet={imageUrl}
                 alt={item.alt}
